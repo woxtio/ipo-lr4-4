4b501f74ac2b01807b1dc10ba1823ba4f50c8c94
dasha <dpaseka7@gmail.com>
2024-10-19 08:56:47 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+strings = ["apple", "banana", "cherry"] #Исходный список строк
+tripled_strings = [s * 3 for s in strings] #Генератор списка, который повторяет каждую строку трижды
+print(tripled_strings) #Вывод результата
